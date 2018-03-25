0c56c1c804abaef0314fbfbfce9ae3c376eaa734
c8t12h19 <hol12062@byui.edu>
2018-03-24 18:10:03 -0600

    Updated index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,39 +1,35 @@
-authenticate();
+var APP_KEY = 'a02f100000STXfVAAX';
+var REDIRECT_URI = 'http://localhost:5000/index.html';
+//var REDIRECT_URI = 'https://intense-plateau-59140.herokuapp.com/';
 
-function authenticate() {
-  var APP_KEY = 'a02f100000STXfVAAX';
-  var REDIRECT_URI = 'http://localhost:5000/index.html';
-  //var REDIRECT_URI = 'https://intense-plateau-59140.herokuapp.com/';
+// Create and configure the API client
+var familysearch = new FamilySearch({
+  environment: 'integration',
+  appKey: APP_KEY,
+  redirectUri: REDIRECT_URI,
+  saveAccessToken: true
+});
 
-  // Create and configure the API client
-  var familysearch = new FamilySearch({
-    environment: 'integration',
-    appKey: APP_KEY,
-    redirectUri: REDIRECT_URI,
-    saveAccessToken: true
+// Process OAuth response, if we have one. true is returned if a code parameter
+// was found in the query. Otherwise false is returned.
+if(familysearch.oauthResponse(load)){
+  // When oauthResponse returns true it means it found a code and is
+  // going to exchange it for an access token. The response is handled in
+  // the callback parameter of that method.
+}
+
+// We didn't find a oauth code in the query parameters so now we check to see
+// if we're already authenticated.
+else if(familysearch.getAccessToken()){
+  load();
+}
+
+// If we're not processing an OAuth response and we're not already authenticated,
+// we wire up and display the sign in button so that the user can initiate authentication.
+else {
+  document.getElementById('signin').addEventListener('click', function(){
+    familysearch.oauthRedirect();
   });
-
-  // Process OAuth response, if we have one. true is returned if a code parameter
-  // was found in the query. Otherwise false is returned.
-  if(familysearch.oauthResponse(load)){
-    // When oauthResponse returns true it means it found a code and is
-    // going to exchange it for an access token. The response is handled in
-    // the callback parameter of that method.
-  }
-
-  // We didn't find a oauth code in the query parameters so now we check to see
-  // if we're already authenticated.
-  else if(familysearch.getAccessToken()){
-    load();
-  }
-
-  // If we're not processing an OAuth response and we're not already authenticated,
-  // we wire up and display the sign in button so that the user can initiate authentication.
-  else {
-    document.getElementById('signin').addEventListener('click', function(){
-      familysearch.oauthRedirect();
-    });
-  }
 }
 
 /**
